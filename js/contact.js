d1aa2d6d51150371ec093f0f6d5a29bb3aef3cd5
Nnamdi Ezeh <nnamdiezeh0@gmail.com>
2022-08-19 14:54:01 +0100

    first commit

---
+++ js/contact.js
@@ -0,0 +1,24 @@
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+});
+
+
+
+function onLoad(params) {
+
+    var tempParams = {
+        from_name: document.getElementById("name").value,
+        email_id: document.getElementById("email").value,
+        message: document.getElementById("message").value
+    };
+
+
+
+    emailjs.send("service_5rg4jzd", "template_d3n7vmw", tempParams)
+        .then(function (res) {
+            console.log("ok", res.status);
+        })
+}
