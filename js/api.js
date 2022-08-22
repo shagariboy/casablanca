dbceca7985acb29938db1541fd7212d56b0d8a7a
Nnamdi Ezeh <nnamdiezeh0@gmail.com>
2022-08-22 20:07:32 +0100

    added new pages and api script

---
+++ js/api.js
@@ -0,0 +1,31 @@
+fetch("https://reqres.in/api/users?page=2")
+    .then((response) => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error("NETWORK RESPONSE ERROR");
+        }
+    })
+    .then(data => {
+        console.log(data);
+        displayUsers(data)
+    })
+    .catch((error) => console.error("FETCH ERROR:", error));
+
+
+function displayUsers(data) {
+    let tab =
+        `<h1></h1>
+       <h1></h1>
+       `;
+
+
+    for (let r of data.data) {
+        tab += `
+        <h1>${r.first_name}&nbsp;${r.last_name}</h1>
+        <p>${r.email}</p>
+        `;
+    }
+
+    document.getElementById("users").innerHTML = tab;
+}
