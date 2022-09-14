4313a6b9a0fc38bdb65dc0de9d71c6e8ae004806
Nnamdi Ezeh <nnamdiezeh0@gmail.com>
2022-09-14 12:32:06 +0100

    making code clean

---
+++ js/time.js
@@ -0,0 +1,17 @@
+function updateClock() {
+    var now = new Date(), // current date
+        months = ['January', 'February', '...']; // you get the idea
+    time = now.getHours() + ':' + ("0" + now.getMinutes()).slice(-2), // again, you get the idea
+
+        // a cleaner way than string concatenation
+        date = [now.getDate(),
+        months = [now.getMonth()],
+        now.getFullYear()].join(' ');
+
+    // set the content of the element with the ID time to the formatted string
+    document.getElementById('time').innerHTML = [date, time].join('  / ');
+
+    // call this function again in 1000ms
+    setTimeout(updateClock, 1000);
+}
+updateClock(); // initial call
