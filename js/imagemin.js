50e3b351034ab1072ca76c0b8fbcba6a60d7a17c
Nnamdi Ezeh <nnamdiezeh0@gmail.com>
2022-09-19 09:38:29 +0100

    edits to improve page performance

--- js/imagemin.js
+++ js/imagemin.js
@@ -1,13 +1,13 @@
-import imagemin from 'imagemin';
+
 import imageminWebp from 'imagemin-webp';
 
 (async () => {
-	await imagemin(['./public/image/*.{jpg,png}'], {
-		destination: 'build/images',
-		plugins: [
-			imageminWebp({quality: 50})
-		]
-	});
+    await imagemin(['./public/image/*.{jpg,png}'], {
+        destination: 'build/images',
+        plugins: [
+            imageminWebp({ quality: 50 })
+        ]
+    });
 
-	console.log('Images optimized');
+    console.log('Images optimized');
 })();
